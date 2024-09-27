9e49d500401009d59d0c83c5bbb133420d6db1c4
ADINAN-SUWANNOCHIN <aotadinan.s@gmail.com>
2024-09-27 20:32:40 +0700

    fixed  bug

--- src/index.js
+++ src/index.js
@@ -1,31 +1,72 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/index.ts
-const library_1 = require("./library");
-const models_1 = require("./models");
-const library = new library_1.Library();
-// Adding some books for testing
-library.addBook({
-    title: "1984",
-    author: "George Orwell",
-    genre: models_1.Genre.Fiction,
-    publishedYear: 1949,
-    isAvailable: true,
-});
-library.addBook({
-    title: "A Brief History of Time",
-    author: "Stephen Hawking",
-    genre: models_1.Genre.Science,
-    publishedYear: 1988,
-    isAvailable: true,
-});
-// Listing all books
-library.listBooks();
-// Searching for a book by title
-console.log(library.searchBooks("title", "1984"));
-// Updating a book
-library.updateBook(1, { author: "Orwell" });
-// Deleting a book
-library.deleteBook(2);
-// Listing all books after deletion
-library.listBooks();
+// สร้างclassชื่อLibraryใช้จัดการหนังสือ
+var Library = /** @class */ (function () {
+    function Library() {
+        this.books = [];
+    }
+    Library.prototype.addBook = function (book) {
+        this.books.push(book);
+        console.log("".concat(book.title, " added to library."));
+    };
+    Library.prototype.listBooks = function () {
+        console.log("List of books in the library:");
+        this.books.forEach(function (book) { return console.log("- ".concat(book.title, " by ").concat(book.author)); });
+    };
+    Library.prototype.searchBooks = function (field, value) {
+        var results = this.books.filter(function (book) { return book[field] === value; });
+        if (results.length > 0) {
+            console.log("Found: ".concat(results[0].title, " by ").concat(results[0].author));
+        }
+        else {
+            console.log("No book found with the given title.");
+        }
+    };
+    Library.prototype.updateBook = function (id, updatedFields) {
+        var book = this.books.find(function (b) { return b.id === id; });
+        if (book) {
+            Object.assign(book, updatedFields);
+            console.log("Book with id ".concat(id, " updated to: ").concat(JSON.stringify(book)));
+        }
+        else {
+            console.log("Book with id ".concat(id, " not found for update."));
+        }
+    };
+    Library.prototype.deleteBook = function (id) {
+        var initialLength = this.books.length;
+        this.books = this.books.filter(function (book) { return book.id !== id; });
+        if (this.books.length < initialLength) {
+            console.log("Book with id ".concat(id, " deleted."));
+        }
+        else {
+            console.log("Book with id ".concat(id, " not found for deletion."));
+        }
+    };
+    // สร้างฟังกืชั่นไว้ทดสอบระบบ
+    Library.prototype.testLibrary = function () {
+        // เพิ่มหนังสือ
+        this.addBook({ title: "To Kill a Mockingbird", author: "Harper Lee", genre: "Fiction", publishedYear: 1960, isAvailable: true, id: 3 });
+        this.addBook({ title: "1984", author: "George Orwell", genre: "Fiction", publishedYear: 1949, isAvailable: true, id: 1 });
+        this.addBook({ title: "A Brief History of Time", author: "Stephen Hawking", genre: "Science", publishedYear: 1988, isAvailable: true, id: 2 });
+        // แสดงหนังสือทั้งหมด
+        this.listBooks();
+        // ค้นหาหนังสือ
+        console.log("Search for a book by title:");
+        this.searchBooks("title", "1984");
+        // อัปเดตข้อมูลผู้แต่ง
+        console.log("Update a book's author:");
+        this.updateBook(1, { author: "Orwell" });
+        var updatedBook = this.searchBooks("id", 1);
+        // ลบหนังสือ
+        console.log("Delete a book from the library:");
+        this.deleteBook(2);
+        console.log("Current books in library after deletion:");
+        this.listBooks();
+        // ลบหนังสือที่ไม่อยู่ในลิสต์
+        console.log("Delete a non-existent book:");
+        var bookIdToDelete = 999;
+        this.deleteBook(bookIdToDelete);
+    };
+    return Library;
+}());
+// สร้างตัวอย่างของLibraryและเรียกฟังก์ชันทดสอบ
+var libraryTest = new Library();
+libraryTest.testLibrary();
--- src/library.js
+++ src/library.js
@@ -1,45 +1,57 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Library = void 0;
-class Library {
-    constructor() {
+var Library = /** @class */ (function () {
+    function Library() {
         this.books = [];
         this.nextId = 1;
     }
-    addBook(book) {
-        const newBook = Object.assign(Object.assign({}, book), { id: this.nextId++ });
+    Library.prototype.addBook = function (book) {
+        var newBook = __assign(__assign({}, book), { id: this.nextId++ });
         this.books.push(newBook);
         return newBook;
-    }
+    };
     // src/library.ts (add to the Library class)
-    listBooks() {
+    Library.prototype.listBooks = function () {
         if (this.books.length === 0) {
             console.log("No books in the inventory.");
         }
         else {
-            this.books.forEach(book => console.log(book));
+            this.books.forEach(function (book) { return console.log(book); });
         }
-    }
+    };
     // src/library.ts (add to the Library class)
-    searchBooks(property, value) {
-        return this.books.filter(book => book[property] === value);
-    }
+    Library.prototype.searchBooks = function (property, value) {
+        return this.books.filter(function (book) { return book[property] === value; });
+    };
     // src/library.ts (add to the Library class)
-    updateBook(id, updates) {
-        const book = this.books.find(b => b.id === id);
+    Library.prototype.updateBook = function (id, updates) {
+        var book = this.books.find(function (b) { return b.id === id; });
         if (book) {
             Object.assign(book, updates);
         }
         return book;
-    }
+    };
     // src/library.ts (add to the Library class)
-    deleteBook(id) {
-        const index = this.books.findIndex(book => book.id === id);
+    Library.prototype.deleteBook = function (id) {
+        var index = this.books.findIndex(function (book) { return book.id === id; });
         if (index !== -1) {
             this.books.splice(index, 1);
             return true;
         }
         return false;
-    }
-}
+    };
+    return Library;
+}());
 exports.Library = Library;
